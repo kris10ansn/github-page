54c4dc3ce8c2c28d3618dd17d5420a3601e963c1
BuildTools <unconfigured@null.spigotmc.org>
2020-04-01 21:18:02 +0200

    merge Projects and SectionTitle

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,35 +1,52 @@
 import React from "react";
 import projects from "../assets/projects/projects.json";
 import "./Projects.scss";
+import github from "../assets/github.svg";
 
 export default function Projects() {
     return (
-        <div className="projects">
-            {projects.map(({ title, description, buttons, image }, index) => {
-                const src = require(`../assets/projects/${image}`);
+        <section name="projects">
+            <div className="title">
+                <a
+                    href="https://github.com/kris10ansn"
+                    title="projects"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img src={github} alt="Loading..." />
+                </a>
+                <h1>Projects</h1>
+            </div>
 
-                return (
-                    <div className="card" key={index}>
-                        <div className="info">
-                            <img src={src} alt={title} />
-                            <h1>{title}</h1>
-                            <p>{description}</p>
-                        </div>
-                        <div className="buttons">
-                            {buttons.map(({ text, url }, index) => (
-                                <a
-                                    href={url}
-                                    target="_blank"
-                                    key={index}
-                                    rel="noopener noreferrer"
-                                >
-                                    <button>{text}</button>
-                                </a>
-                            ))}
-                        </div>
-                    </div>
-                );
-            })}
-        </div>
+            <div className="projects">
+                {projects.map(
+                    ({ title, description, buttons, image }, index) => {
+                        const src = require(`../assets/projects/${image}`);
+
+                        return (
+                            <div className="card" key={index}>
+                                <div className="info">
+                                    <img src={src} alt={title} />
+                                    <h1>{title}</h1>
+                                    <p>{description}</p>
+                                </div>
+                                <div className="buttons">
+                                    {buttons.map(({ text, url }, index) => (
+                                        <a
+                                            href={url}
+                                            target="_blank"
+                                            key={index}
+                                            rel="noopener noreferrer"
+                                        >
+                                            <button>{text}</button>
+                                        </a>
+                                    ))}
+                                </div>
+                            </div>
+                        );
+                    }
+                )}
+            </div>
+        </section>
     );
 }
