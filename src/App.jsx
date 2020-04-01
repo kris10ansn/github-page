cb9cecfae61f2e650ab41ffd3898eb66b8ca54a4
BuildTools <unconfigured@null.spigotmc.org>
2020-04-01 21:21:25 +0200

    remove SectionTitle

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import "./App.scss";
-import github from "./assets/github.svg";
 import React from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-import SectionTitle from "./components/SectionTitle";
 import Projects from "./components/Projects";
 
 function App() {
@@ -11,11 +9,6 @@ function App() {
         <div className="App">
             <Navbar></Navbar>
             <Header></Header>
-            <SectionTitle
-                title="projects"
-                image={github}
-                url="https://github.com/kris10ansn"
-            ></SectionTitle>
             <Projects></Projects>
         </div>
     );
