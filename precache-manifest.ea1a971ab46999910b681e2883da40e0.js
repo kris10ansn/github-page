734e9573c6c6042d5c2f9b9f75c7bea31be8e81c
kris10ansn <40371637+kris10ansn@users.noreply.github.com>
2020-04-02 20:36:05 +0200

    Updates

---
+++ precache-manifest.ea1a971ab46999910b681e2883da40e0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44c51d82f90182fa8847a591d3a4d5fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "626f07e0823f5ac2ac33",
+    "url": "/static/css/main.fd8be75a.chunk.css"
+  },
+  {
+    "revision": "16231e72a1bd8e25568d",
+    "url": "/static/js/2.3156b4aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3156b4aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "626f07e0823f5ac2ac33",
+    "url": "/static/js/main.53813ed7.chunk.js"
+  },
+  {
+    "revision": "3eece586bb1ae4c6104b",
+    "url": "/static/js/runtime-main.b8367cfe.js"
+  },
+  {
+    "revision": "1ce9abde93009b96652cd71f975881bb",
+    "url": "/static/media/code.1ce9abde.png"
+  },
+  {
+    "revision": "9742d8419464b36c063e781f162de455",
+    "url": "/static/media/github.9742d841.svg"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/static/media/hamburger.86595150.svg"
+  },
+  {
+    "revision": "1306cd7019383ab23161fe3fc2e8524d",
+    "url": "/static/media/microbit.1306cd70.png"
+  },
+  {
+    "revision": "1813302af5f1978db8d848d962439984",
+    "url": "/static/media/ukens_timeplan.1813302a.png"
+  }
+]);
