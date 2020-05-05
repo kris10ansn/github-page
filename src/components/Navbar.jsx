05a25826ff0381dc4ce764eafbb7eb75a7142d6c
kris10ansn <40371637+kris10ansn@users.noreply.github.com>
2020-05-05 17:50:59 +0200

    Remove project anchor, add GitHub link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./Navbar.scss";
-import { Link } from "react-scroll";
 import hamburger from "../assets/hamburger.svg";
 
 export default function Navbar() {
@@ -13,10 +12,8 @@ export default function Navbar() {
             </div>
 
             <div className="menu">
+                <a href="https://github.com/kris10ansn">GitHub</a>
                 <a href="mailto:kristian.nessa@protonmail.com">Contact</a>
-                <Link to="projects" smooth={true} duration={500}>
-                    Projects
-                </Link>
 
                 <a
                     href="https://www.buymeacoffee.com/klUfyKN"
